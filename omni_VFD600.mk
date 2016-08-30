11b69c18b46c2f6d6586633cb51cebe9cb497b77
Wzedlare <vedatak01@gmail.com>
2016-08-30 11:43:56 +0300

    Add more links to ramdisk

--- omni_VFD600.mk
+++ omni_VFD600.mk
@@ -29,11 +29,13 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/root/init.qcom.usb.rc:recovery/root/init.qcom.usb.rc \
     $(LOCAL_PATH)/rootdir/root/init.target.rc:recovery/root/init.target.rc \
     $(LOCAL_PATH)/rootdir/root/ueventd.qcom.rc:recovery/root/ueventd.qcom.rc \
-    $(LOCAL_PATH)/rootdir/root/init.class.main.sh:recovery/root/init.class_main.sh \
+    $(LOCAL_PATH)/rootdir/root/init.class_main.sh:recovery/root/init.class_main.sh \
     $(LOCAL_PATH)/rootdir/root/init.mdm.sh:recovery/root/init.mdm.sh \
     $(LOCAL_PATH)/rootdir/root/init.qcom.bms.sh:recovery/root/init.qcom.bms.sh \
     $(LOCAL_PATH)/rootdir/root/init.qcom.class_core.sh:recovery/root/init.qcom.class_core.sh \
     $(LOCAL_PATH)/rootdir/root/init.qcom.early_boot.sh:recovery/root/init.qcom.early_boot.sh \
     $(LOCAL_PATH)/rootdir/root/init.qcom.sh:recovery/root/init.qcom.sh \
     $(LOCAL_PATH)/rootdir/root/init.qcom.syspart_fixup.sh:recovery/root/init.qcom.syspart_fixup.sh \
-    $(LOCAL_PATH)/rootdir/root/init.qcom.usb_zte.sh:recovery/root/init.qcom.usb_zte.sh
+    $(LOCAL_PATH)/rootdir/root/init.qcom.usb_zte.sh:recovery/root/init.qcom.usb_zte.sh \
+    $(LOCAL_PATH)/rootdir/root/init.qcom.usb.sh:recovery/root/init.qcom.usb.sh \
+    $(LOCAL_PATH)/rootdir/root/init.rc:recovery/root/init.rc
