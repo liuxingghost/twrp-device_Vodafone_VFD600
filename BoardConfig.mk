e5c32c24e3e87417a884c438e8ae856e651cd2e8
Wzedlare <vedatak01@gmail.com>
2016-08-31 18:39:45 +0300

    Use prebuilt kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -76,8 +76,7 @@ BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.selinux=permissive androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS :=  --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --kernel_offset 0x00008000
-TARGET_KERNEL_CONFIG := msm8909-1gb_P809V50_defconfig
-TARGET_KERNEL_SOURCE := kernel/Vodafone/VFD600/del_kernel
+TARGET_PREBUILT_KERNEL := device/Vodafone/VFD600/prebuilt/kernel
 
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
